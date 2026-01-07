2deaa8744c10b0bdb452462fdd583b66e7955704
Carysaurus <carysrichards@live.com>
2026-01-07 10:00:01 -0700

    added note colour themes

--- config/colourThemes.js
+++ config/colourThemes.js
@@ -1,6 +1,6 @@
 // colourThemes.js
 
-const boxColours = ['light', 'pink', 'blue', 'dark']
+const boxColours = ['light', 'pink', 'blue', 'green', 'orange', 'purple', 'teal']
 const noteColours = ['light', 'pink', 'blue', 'dark']
 
 module.exports = {
